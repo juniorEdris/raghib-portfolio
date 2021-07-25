cdd0163ec5fbf983307bfe7afc97d33f08d355ab
eftekarRaghib <mdraghib17@gmail.com>
2021-07-26 01:21:38 +0600

    email fixed

--- Components/Header/ContactDetails.js
+++ Components/Header/ContactDetails.js
@@ -4,7 +4,7 @@ const ContactDetails = () => {
     return ( 
         <div className=" flex items-center justify-center md:justify-end text-sm">
             <span className={`mr-3 ${classes.contact_span}`}>+8801846029691</span>
-            <span className={`${classes.contact_span}`}>mohammedimthiaz17@gmail.com</span>
+            <span className={`${classes.contact_span}`}>mdraghib17@gmail.com</span>
         </div>
      );
 }
